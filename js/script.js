d8af4283d629d4282f127eb564ff9b9c8ce7e602
Giosuè Lentini <giosuelentini94@outlook.com>
2020-05-19 16:20:48 +0200

    variabili

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var nomeUtente = prompt('Inserisci il tuo nome');
+console.log('nomeUtente'); // cancellare
+var cognomeUtente = prompt('Inserisci il tuo cognome');
+console.log('cognomeUtente'); // cancellare
+
+document.getElementById('password').innerHTML = 'La tua password è ';
+console.log('password'); // cancellare
